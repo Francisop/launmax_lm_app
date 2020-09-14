fa218150fc1fdc94bc2fd426dd1266651b122e9f
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-09-14 16:30:57 +0100

    First Sign Up UI implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:launmax_lm_app/ui/constant.dart';
-import 'package:launmax_lm_app/ui/homeScreen/home_page.dart';
-import 'package:launmax_lm_app/ui/homeScreen/home_screen.dart';
-import 'package:launmax_lm_app/ui/screens/viewPricing.dart';
+import 'package:launmax_lm_app/ui/screens/signup/sign_up_1.dart';
+
 import 'package:launmax_lm_app/ui/styles.dart';
 
 void main() {
@@ -14,26 +13,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       title: 'LaunMax DryCleaner',
+      debugShowCheckedModeBanner: false,
+      title: 'LaunMax DryCleaner',
       theme: ThemeData(
-            appBarTheme: AppBarTheme(
-              color: Colors.white,
-              iconTheme: IconThemeData(color: Colors.black),
-              actionsIconTheme: IconThemeData(color: Colors.black),
-              elevation: 1,
-              brightness: Brightness.light, // status bar brightness
-            ),
-            primaryTextTheme: Typography.blackCupertino
-                .copyWith(bodyText2: TextStyle(fontFamily: kSFProText)),
-            primarySwatch: AppColor.primaryColor,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            scaffoldBackgroundColor: AppColor.background,
-            textTheme: Typography.blackCupertino
-                .copyWith(bodyText2: TextStyle(fontFamily: kSFProText)),
-          ),
-      home: HomeScreen(),
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          actionsIconTheme: IconThemeData(color: Colors.black),
+          elevation: 1,
+          brightness: Brightness.light, // status bar brightness
+        ),
+        primaryTextTheme: Typography.blackCupertino
+            .copyWith(bodyText2: TextStyle(fontFamily: kSFProText)),
+        primarySwatch: AppColor.primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: AppColor.background,
+        textTheme: Typography.blackCupertino
+            .copyWith(bodyText2: TextStyle(fontFamily: kSFProText)),
+      ),
+      home: SignUp1(),
     );
   }
 }
-
--- lib/ui/constant.dart
+++ lib/ui/constant.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:launmax_lm_app/ui/styles.dart';
 
+import 'styles.dart';
 
 class Constants {}
 
@@ -35,3 +36,21 @@ final kBoxShadow = <BoxShadow>[
     color: Colors.black26,
   )
 ];
+
+final headlineStyle = TextStyle(
+  fontSize: 28.0,
+  fontWeight: FontWeight.w600,
+  color: AppColor.neutralBlack,
+);
+
+final descriptionStyle = TextStyle(
+  fontSize: 17.0,
+  fontWeight: FontWeight.w300,
+  color: AppColor.text2,
+);
+
+final buttonTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w300,
+  color: AppColor.primaryColor,
+);
