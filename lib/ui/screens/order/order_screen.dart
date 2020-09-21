67586f9059e66464cd6565f9d763569ef25836fe
Francisop <francisohis@gmail.com>
2020-09-21 13:55:57 +0100

    chore:order screen 20%

--- lib/ui/screens/order/order_screen.dart
+++ lib/ui/screens/order/order_screen.dart
@@ -58,35 +58,14 @@ class _OrderScreenState extends State<OrderScreen> {
               ListView(
                 children: [
                   MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
+                    items: ["Agbada", "Buba", "Sokoto"],
+                    value: 1000,
+                    orderNo: "Order No1947034",
+                    quantity: 2,
                     tap: null,
+                    status: "pending",
                   ),
-                  MyCards(
-                    heading: "Agbada (only)",
-                    subHeading: 'NGN 500',
-                    color: Colors.white,
-                    image: 'assets/images/product2.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Socks",
-                    subHeading: 'NGN 10',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product3.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
-                    tap: null,
-                  ),
+                
                   SizedBox(
                     height: 26,
                   ),
@@ -95,35 +74,14 @@ class _OrderScreenState extends State<OrderScreen> {
               ListView(
                 children: [
                   MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
+                    items: ["Agbada", "Buba", "Sokoto"],
+                    value: 1000,
+                    orderNo: "Order No1947034",
+                    quantity: 2,
                     tap: null,
+                    status: "pending",
                   ),
-                  MyCards(
-                    heading: "Agbada (only)",
-                    subHeading: 'NGN 500',
-                    color: Colors.white,
-                    image: 'assets/images/product2.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Socks",
-                    subHeading: 'NGN 10',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product3.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
-                    tap: null,
-                  ),
+                
                   SizedBox(
                     height: 26,
                   ),
@@ -132,35 +90,14 @@ class _OrderScreenState extends State<OrderScreen> {
               ListView(
                 children: [
                   MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
+                    items: ["Agbada", "Buba", "Sokoto"],
+                    value: 1000,
+                    orderNo: "Order No1947034",
+                    quantity: 2,
                     tap: null,
+                    status: "pending",
                   ),
-                  MyCards(
-                    heading: "Agbada (only)",
-                    subHeading: 'NGN 500',
-                    color: Colors.white,
-                    image: 'assets/images/product2.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Socks",
-                    subHeading: 'NGN 10',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product3.png',
-                    tap: null,
-                  ),
-                  SizedBox(height: 15),
-                  MyCards(
-                    heading: "Agbada, Buba and Sokoto",
-                    subHeading: 'NGN 1000',
-                    color: Theme.of(context).accentColor,
-                    image: 'assets/images/product1.png',
-                    tap: null,
-                  ),
+                
                   SizedBox(
                     height: 26,
                   ),
@@ -175,17 +112,19 @@ class _OrderScreenState extends State<OrderScreen> {
 }
 
 class MyCards extends StatelessWidget {
-  final String heading;
-  final String subHeading;
-  final String image;
-  final Color color;
+  final String status;
+  final String orderNo;
+  final int value;
+  final int quantity;
+  final List items;
   final Function tap;
   const MyCards({
     Key key,
-    this.heading,
-    this.subHeading,
-    this.image,
-    this.color,
+    this.value,
+    this.orderNo,
+    this.quantity,
+    this.items,
+    this.status,
     this.tap,
   }) : super(key: key);
   @override
@@ -194,68 +133,52 @@ class MyCards extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
       onTap: tap,
       child: Card(
-        elevation: 0,
-        color: Colors.transparent,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Stack(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
+          elevation: 9.9,
+          color: Colors.transparent,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Column(children: [
+            Container(
+                child: Row(
               children: [
-                Image(
-                  image: AssetImage(image),
-                  height: 235,
-                  width: McGyver.rsDoubleW(context, 140),
+                Container(
+                  child: Row(children: [
+                    Icon(Icons.timer),
+                    Text(status.toUpperCase()),
+                  ]),
                 ),
+                Container(
+                  child: Text("sep 06, 2020 03:36 PM"),
+                )
               ],
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              top: 100,
-              bottom: 0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //  crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 56,
+            )),
+            Divider(),
+            Container(
+              child: Column(children: [
+                Container(child: Text(orderNo)),
+                Container(
+                  child: Row(
+                    children: [
+                      Text('item'),
+                      Text(items.toString()),
+                    ],
                   ),
-                  Container(
-                    width: 370,
-                    padding: EdgeInsets.only(top: 20, bottom: 20),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10)),
-                        color: Colors.white),
-                    child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            heading,
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xff151522),
-                            ),
-                          ),
-                          Text(
-                            subHeading,
-                            style: TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ]),
-                  ),
-                ],
-              ),
+                )
+              ]),
             ),
-          ],
-        ),
-      ),
+            Divider(),
+            Container(
+              child: Row(children: [
+                Text('VALUE OF ITEMS'),
+                Text('NGN $value'),
+                Text('QUANTITY'),
+                Text(quantity.toString()),
+              ]),
+            ),
+          ])),
     );
   }
 }
+
+
+// Theme.of(context).accentColor
